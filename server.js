f74cb371e135abc8da7fe0e08ba9a4ba83ffb05c
짝은 빵 <song@MacBook-Pro.local>
2026-07-22 00:20:51 +0900

    fix start script for render

--- server.js
+++ server.js
@@ -97,3 +97,9 @@ const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`🚀 [로컬 서버 실행 중] http://localhost:${PORT}`);
 });
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
